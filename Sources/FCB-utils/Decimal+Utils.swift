8badd065b848cc57398123385644bd33ab4fc95e
Denis Sancov <denis.sancov@gmail.com>
2021-04-11 12:06:33 +0300

    Improvements

---
+++ Sources/FCB-utils/Decimal+Utils.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Denis Sancov on 4/11/21.
+//
+
+import Foundation
+
+public extension Decimal {
+    var asNumber: NSDecimalNumber {
+        return self as NSDecimalNumber
+    }
+}
