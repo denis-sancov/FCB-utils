3cdd57860ecf2b6f86e4222246281485174a77bc
Denis Sancov <denis.sancov@gmail.com>
2021-04-23 21:27:30 +0300

    improved state model

--- Sources/FCB-utils/Model/State.swift
+++ Sources/FCB-utils/Model/State.swift
@@ -7,17 +7,69 @@
 
 import Foundation
 
-public enum State<T> {
-    case loading
-    case success(rows: [T])
-    case error(err: Error)
+public typealias EmptyState = State<Void>
 
-    public var rows: [T]? {
+public enum State<T> {
+    case success(T)
+    case loading(T?)
+    case failed(err: Error, T?)
+
+    public var data: T? {
         switch self {
-        case .success(let rows):
-            return rows
-        default:
-            return nil
+        case .success(let data):
+            return data
+        case .loading(let data), .failed(_, let data):
+            return data
+        }
+    }
+
+    public var isSuccess: Bool {
+        if case .success = self {
+            return true
+        }
+        return false
+    }
+
+    public var isLoading: Bool {
+        if case .loading = self {
+            return true
+        }
+        return false
+    }
+
+    public var error: Error? {
+        if case .failed(let err, _ ) = self {
+            return err
+        }
+        return nil
+    }
+
+    public var hasData: Bool {
+        return self.data != nil
+    }
+
+    public var asVoid: State<Void> {
+        switch self {
+        case .success:
+            return .success(())
+        case .loading:
+            return .loading(nil)
+        case .failed(let err, _):
+            return .failed(err: err, nil)
+        }
+    }
+
+
+    public func map<A>(transform: (T?) -> A) -> State<A> {
+        let result = transform(data)
+
+        switch self {
+        case .success:
+            return .success(result)
+        case .loading:
+            return .loading(result)
+        case .failed(let err, _):
+            return .failed(err: err, result)
         }
     }
 }
